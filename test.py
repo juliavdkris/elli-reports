54448cee25dedcb2480fe334d11dd57d5fb6b2ee
Julia van der Kris <julia@juuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuulia.dev>
2023-07-15 16:34:46 +0200

    More cleaning + nice dataclass for cell values

--- test.py
+++ test.py
@@ -2,11 +2,24 @@ from odf.opendocument import OpenDocument, load
 from odf.element import Element
 from odf.chart import Chart
 from odf.table import Table, TableRows, TableRow, TableCell
+from dataclasses import dataclass
 from pprint import pprint
 
 
-# Type aliases
-CellValue = tuple[str|None, str|None] | None
+@dataclass
+class CellValue:
+	value: str | None
+	type: str | None
+
+	def __str__(self) -> str:
+		if self.value is None:
+			return 'NONE'
+		if self.type == 'float':
+			return f'({float(self.value):.2f}, {self.type})'
+		else:
+			return f'(\'{self.value}\', {self.type})'
+	__repr__ = __str__
+
 ChartData = list[list[CellValue]]
 
 
@@ -16,16 +29,11 @@ def get_charts(doc: OpenDocument) -> list[Element]:
 
 def parse_cell(cell: Element) -> CellValue:
 	if cell.getAttribute('value'):
-		return (cell.getAttribute('value'), cell.attributes.get(('urn:oasis:names:tc:opendocument:xmlns:office:1.0', 'value-type')))
+		return CellValue(cell.getAttribute('value'), cell.attributes.get(('urn:oasis:names:tc:opendocument:xmlns:office:1.0', 'value-type')))
 	elif len(cell.attributes) >= 1  and len(cell.childNodes[0].childNodes) >= 1:
-		return (cell.childNodes[0].childNodes[0].data, 'string')
+		return CellValue(cell.childNodes[0].childNodes[0].data, 'string')
 	else:
-		return None
-
-
-def dump_all_charts(doc: OpenDocument) -> list[ChartData]:
-	charts = get_charts(doc)
-	return [dump_chart(doc, i) for i in range(len(charts))]
+		return CellValue(None, None)
 
 
 def dump_chart(doc: OpenDocument, index: int) -> ChartData:
@@ -33,13 +41,18 @@ def dump_chart(doc: OpenDocument, index: int) -> ChartData:
 	return [[parse_cell(cell) for cell in row.getElementsByType(TableCell)] for row in chart.getElementsByType(TableRow)]
 
 
+def dump_all_charts(doc: OpenDocument) -> list[ChartData]:
+	charts = get_charts(doc)
+	return [dump_chart(doc, i) for i in range(len(charts))]
+
+
 def write_chart(doc: OpenDocument, index: int, data: ChartData) -> None:
 	chart = get_charts(doc)[index]
 	for row, new_row in zip(chart.getElementsByType(TableRow), data):
-		for cell, new_value in zip(row.getElementsByType(TableCell), new_row):
-			if new_value and new_value[1] == 'float' and len(cell.attributes) >= 2 and cell.getAttribute('value') != new_value[0]:
+		for cell, new_cell in zip(row.getElementsByType(TableCell), new_row):
+			if new_cell.type == 'float' and len(cell.attributes) >= 2 and cell.getAttribute('value') != new_cell.value:
 				# TODO: also edit the text value?
-				cell.setAttribute('value', new_value[0])
+				cell.setAttribute('value', new_cell.value)
 	doc.save('original/template2.odt')
 
 
@@ -50,5 +63,5 @@ if __name__ == '__main__':
 
 	# data = dump_chart(doc, 0)
 	# pprint(data)
-	# data[1][2] = '999'
+	# data[1][2].value = '999'
 	# write_chart(doc, 0, data)
