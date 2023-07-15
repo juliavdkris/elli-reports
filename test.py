73fc780f3893baad0beba7991a1c50b6f6d5832c
Julia van der Kris <julia@juuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuulia.dev>
2023-07-15 16:12:04 +0200

    Minor cleaning up

--- test.py
+++ test.py
@@ -5,35 +5,39 @@ from odf.table import Table, TableRows, TableRow, TableCell
 from pprint import pprint
 
 
-CellValue = tuple[str|None, str|None]
+# Type aliases
+CellValue = tuple[str|None, str|None] | None
 ChartData = list[list[CellValue]]
 
 
+def get_charts(doc: OpenDocument) -> list[Element]:
+	return [obj for obj in doc.childobjects if obj.getMediaType() == 'application/vnd.oasis.opendocument.chart']
+
+
 def parse_cell(cell: Element) -> CellValue:
-	try:
-		return (cell.getAttribute('value'), cell.getAttribute('value-type'))
-	except:
-		if len(cell.childNodes) == 0 or len(cell.childNodes[0].childNodes) == 0:
-			return (None, None)
-		else:
-			return (cell.childNodes[0].childNodes[0].data, 'string')
+	if cell.getAttribute('value'):
+		return (cell.getAttribute('value'), cell.attributes.get(('urn:oasis:names:tc:opendocument:xmlns:office:1.0', 'value-type')))
+	elif len(cell.attributes) >= 1  and len(cell.childNodes[0].childNodes) >= 1:
+		return (cell.childNodes[0].childNodes[0].data, 'string')
+	else:
+		return None
 
 
 def dump_all_charts(doc: OpenDocument) -> list[ChartData]:
-	charts = [obj for obj in doc.childobjects if obj.getMediaType() == 'application/vnd.oasis.opendocument.chart']
+	charts = get_charts(doc)
 	return [dump_chart(doc, i) for i in range(len(charts))]
 
 
 def dump_chart(doc: OpenDocument, index: int) -> ChartData:
-	chart = [obj for obj in doc.childobjects if obj.getMediaType() == 'application/vnd.oasis.opendocument.chart'][index]
+	chart = get_charts(doc)[index]
 	return [[parse_cell(cell) for cell in row.getElementsByType(TableCell)] for row in chart.getElementsByType(TableRow)]
 
 
 def write_chart(doc: OpenDocument, index: int, data: ChartData) -> None:
-	chart = [obj for obj in doc.childobjects if obj.getMediaType() == 'application/vnd.oasis.opendocument.chart'][index]
+	chart = get_charts(doc)[index]
 	for row, new_row in zip(chart.getElementsByType(TableRow), data):
 		for cell, new_value in zip(row.getElementsByType(TableCell), new_row):
-			if len(cell.attributes) >= 2 and cell.getAttribute('value') != new_value:
+			if new_value and new_value[1] == 'float' and len(cell.attributes) >= 2 and cell.getAttribute('value') != new_value[0]:
 				# TODO: also edit the text value?
 				cell.setAttribute('value', new_value[0])
 	doc.save('original/template2.odt')
