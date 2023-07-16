48ac38bc0d491982b73d7dc03fbd86c201a185a9
Julia van der Kris <julia@juuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuulia.dev>
2023-07-16 19:55:31 +0200

    More flexible sheet parsing

--- main.py
+++ main.py
@@ -1,57 +1,15 @@
 import openpyxl
-from docx import Document
 from docxtpl import DocxTemplate
-
-from dataclasses import dataclass
-from typing import Optional
+from docx_charts import Document
 
 SPREADSHEET = 'original/PersRep_Data Pseud.xlsx'
 TEMPLATE = 'original/template.docx'
 OUTPUT_DIR = 'out'
 
 
-@dataclass
-class Entry:
-	new_id: str
-	student_name: str
-	student_email: str
-	student_id: str
-	course: str
-	eb1: Optional[float]
-	mean_eb1_total: Optional[float]
-	eb2: Optional[float]
-	mean_eb2_total: Optional[float]
-	vb1: Optional[float]
-	mean_vb1_total: Optional[float]
-	vb2: Optional[float]
-	mean_vb2_total: Optional[float]
-	ae1: Optional[float]
-	mean_ae1_total: Optional[float]
-	ae2: Optional[float]
-	mean_ae2_total: Optional[float]
-	ae3: Optional[float]
-	mean_ae3_total: Optional[float]
-	ae4: Optional[float]
-	mean_ae4_total: Optional[float]
-	be1: Optional[float]
-	mean_be1_total: Optional[float]
-	be2: Optional[float]
-	mean_be2_total: Optional[float]
-	be3: Optional[float]
-	mean_be3_total: Optional[float]
-	be4: Optional[float]
-	mean_be4_total: Optional[float]
-	ce1: Optional[float]
-	mean_ce1_total: Optional[float]
-	ce2: Optional[float]
-	mean_ce2_total: Optional[float]
-	ce3: Optional[float]
-	mean_ce3_total: Optional[float]
-	ce4: Optional[float]
-	mean_ce4_total: Optional[float]
+Entry = dict[str, str|float]
 
 
-# Parse every row in the sheet as an Entry object
 def parse_sheet_entries(filename: str) -> list[Entry]:
 	wb = openpyxl.load_workbook(filename)
 	sheet = wb[wb.sheetnames[0]]
@@ -59,43 +17,35 @@ def parse_sheet_entries(filename: str) -> list[Entry]:
 
 	entries: list[Entry] = []
 
-	# Join the two sheets together on the student_id column
-	# A lil sketchy but ¯\_(ツ)_/¯
-	for row in sheet.iter_rows(min_row=2, max_row=sheet.max_row, values_only=True):
-		for sid in student_identification.iter_rows(min_row=2, max_row=student_identification.max_row, values_only=True):
-			if row[0] == sid[3]:
-				new_id = sid[3]
-				student_name = sid[0]
-				student_email = sid[1]
-				student_id = sid[2]
-				entries.append(Entry(*[new_id, student_name, student_email, student_id, *row[1:-1]])) # type: ignore
-		else:
-			print(f'[!] No identification found for {row[0]}')
+	cols = [str(cell.value) for cell in sheet[1]] + [str(cell.value) for cell in student_identification[1]]
+	rows = list(sheet.iter_rows(min_row=2, max_row=sheet.max_row, values_only=True))
+	sids = list(student_identification.iter_rows(min_row=2, max_row=student_identification.max_row, values_only=True))
+	for i, (row, sid) in enumerate(zip(rows, sids)):
+		entry: Entry = {}
+		for col, val in zip(cols, row+sid):
+			if col is None or val is None:
+				continue
+			if entry.get(col) and entry[col] != str(val):
+				print(f'[!] Duplicate column name with different values not allowed: {col} on row {i+1} ({entry[col]} != {val})')
+				exit(1)
+			entry[col.lower()] = float(str(val)) if col.startswith('num_') else str(val)
+		entries.append(entry)
+
+	if len(entries) < len(rows):
+		print(f'[!] No identification was found for {len(rows) - len(sids)}/{len(rows)} students')
+
 	return entries
 
 
 def generate_report(entry: Entry, template: str, output_dir: str) -> None:
 	doc = DocxTemplate(template)
-	context = {
-		'new_id': entry.new_id,
-		'student_name': entry.student_name,
-		'student_email': entry.student_email,
-		'student_id': entry.student_id,
-		'course': entry.course,
-	}
+	context = {k: round(v, 2) if isinstance(v, float) else v for k, v in entry.items()}
 	doc.render(context)
-	doc.save(f'{output_dir}/{entry.new_id}.docx')
+	doc.save(f'{output_dir}/{entry.get("new_id")}.docx')
 
-	# doc = Document(f'{output_dir}/{entry.new_id}.docx')
-
-	print(f'[*] Generated report for {entry.new_id} ({entry.student_name})')
+	print(f'[*] Generated report for {entry.get("new_id")} ({entry.get("student_name")})')
 
 
 if __name__ == '__main__':
-	doc = Document('original/PersonalizedReport_DraftV6.docx')
-
-	for i, p in enumerate(doc.paragraphs):
-		print(i, p.text)
-
-	# for entry in parse_sheet_entries(SPREADSHEET):
-		# generate_report(entry, TEMPLATE, OUTPUT_DIR)
+	for entry in parse_sheet_entries(SPREADSHEET):
+		generate_report(entry, TEMPLATE, OUTPUT_DIR)
